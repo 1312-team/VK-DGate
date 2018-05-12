9e6eebce51b4898c9cf4b9cf1b0369efaa4acb00
sergeygorbatyuk171 <sergeygorbatyuk171@gmail.com>
2018-05-13 02:46:10 +0300

    integrating existing frontend

--- VKDgate/settings.py
+++ VKDgate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'VKDgate.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')    ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
